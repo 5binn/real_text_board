1be81b4058beb3bcfc6121528e8c93c9ed22433c
SBS <dhxnejqls13@naver.com>
2023-12-04 15:36:08 +0900

    5th

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -25,19 +25,14 @@ public class App {
                 String content = sc.nextLine().trim();
                 System.out.print("작가 : ");
                 String author = sc.nextLine().trim();
-
                 WiseList wl = new WiseList(count, content, author);
-
                 wiseListList.add(wl);
                 System.out.println(count + "번 명언이 등록되었습니다.");
                 count++;
-
-
             } else if (command.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언\n------------------");
-                for (int i = 0; i <wiseListList.size(); i++) {
-                    WiseList wl = wiseListList.get(i);
-                    System.out.println(wl.getCount()+" / "+wl.getAuthor()+" / " + wl.getContent());
+                for (WiseList ws : wiseListList) {
+                    System.out.println(ws.getCount() + " / " + ws.getAuthor() + " / " + ws.getContent());
                 }
             }
         }
